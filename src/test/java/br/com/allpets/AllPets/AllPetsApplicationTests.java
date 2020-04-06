16721b1bbf4008c2227604b207e12ac591496d22
Wender Batista <wyndy19@hotmail.com>
2020-04-06 19:10:57 -0300

    Atualização com pom

---
+++ src/test/java/br/com/allpets/AllPets/AllPetsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.allpets.AllPets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllPetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
