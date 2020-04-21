f36a6993dce3a4b34334279f68432d2b26ffe6ae
Igor Santos <igor.santos@keycar.com.br>
2020-04-20 22:52:36 -0300

    add session token and login endpoint

--- src/main/java/br/com/allpets/AllPets/controller/LoginController.java
+++ src/main/java/br/com/allpets/AllPets/controller/LoginController.java
@@ -2,13 +2,13 @@ package br.com.allpets.AllPets.controller;
 
 import br.com.allpets.AllPets.model.User;
 import br.com.allpets.AllPets.repository.LoginRepository;
-import br.com.allpets.AllPets.service.LoginService;
-import br.com.allpets.AllPets.service.OwnerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Base64;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -28,5 +28,27 @@ public class LoginController {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(usuarioRepository.saveAndFlush(user));
+    }
+
+    @PostMapping("teste")
+    public ResponseEntity<?>  logar(@RequestBody User user) {
+        User dataUser = usuarioRepository.findByEmail(user.getEmail());
+        String sessionToken = null;
+
+
+        if(dataUser == null || !dataUser.getPassword().equals(user.getPassword())){
+            dataUser = null;
+        }else{
+            sessionToken = dataUser.getIdUser().toString() + ":" + new Date().toString();
+            sessionToken = (Base64.getEncoder().encodeToString(sessionToken.getBytes()));
+        }
+
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .header("sessionToken", sessionToken)
+                .body(dataUser);
+
+
     }
 }
--- src/main/java/br/com/allpets/AllPets/repository/LoginRepository.java
+++ src/main/java/br/com/allpets/AllPets/repository/LoginRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LoginRepository extends JpaRepository<User, Integer> {
+     User findByEmail(String email);
 }
